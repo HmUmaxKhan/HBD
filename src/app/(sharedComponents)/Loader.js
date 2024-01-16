bb7ecbf0167daffdf61120df3e97e28f006edefb
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2024-01-16 22:42:29 +0500

    Making Loader

---
+++ src/app/(sharedComponents)/Loader.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Loader() {
+  return (
+    <div>
+    <section>
+    <video src="smoke.mp4" autoplay muted ></video>
+    <h1>
+        <span>A</span>
+        <span>B</span>
+        <span>H</span>
+        <span>I</span>
+        <span>S</span>
+        <span>H</span>
+        <span>E</span>
+        <span>K</span>
+    </h1>
+</section>
+    </div>
+  )
+}
+
+export default Loader
